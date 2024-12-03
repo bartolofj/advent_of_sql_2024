dd8cc65b675eda9f3e80a1c450149a9b7c0c610d
Fernando Bartolo <git@fbart.net>
2024-12-03 20:17:19 +0000

    Add files via upload

---
+++ 2.sql
@@ -0,0 +1,12 @@
+with t as (
+	select id, chr(value) as value
+	from letters_a a
+	union all
+	select id, chr(value) 
+	from letters_b b
+	order by id asc 
+) 
+select string_agg(value, '') from t
+where value between 'a' and 'z'
+or value in (' ', '!', '"', '(', ')', ',', '-', '.', ':', ';', '?', '''')
+;
